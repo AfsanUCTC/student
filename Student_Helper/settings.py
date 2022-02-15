3e0164ed0f573abaa757505490022e0d7fb39ff5
AfsanUCTC <89940033+AfsanUCTC@users.noreply.github.com>
2022-02-15 23:08:15 +0600

    Update settings.py

--- Student_Helper/settings.py
+++ Student_Helper/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#ydz0gbpzpd@&79+@dx%csx)1d6@0g1)c79c*h!h75_km9de-#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'studenthelper1.herokuapp.com'
